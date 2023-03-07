eb46f652540663830c3e7afa9f73c72233b90606
John Doe <49613009+ShorterTuxedo@users.noreply.github.com>
2023-03-07 22:36:01 +0800

    Update and rename spanishAdvanced.py to germanAdvanced.py

--- germanAdvanced.py
+++ germanAdvanced.py
@@ -4,7 +4,7 @@ import time
 
 ###consts###
 #the location of the microsoft voice, you need to install them in your settings for it to work (just google install spanish voice package)
-spaniards = ["HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_ES-MX_SABINA_11.0", "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_ZH-TW_HANHAN_11.0"]
+deutschen = ["HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_DE-DE_HEDDA_11.0", "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_ZH-TW_HANHAN_11.0"]
 #determines the speed of the voice, higher is faster (wpm)
 newVoiceRate = 125
 #determines the number of questions asked before the program moves onto a new batch of qs
@@ -12,7 +12,7 @@ batchSize = 1
 
 engine = pyttsx3.init()
 engine.setProperty('rate',newVoiceRate)
-engine.setProperty('voice', spaniards[0])
+engine.setProperty('voice', deutschen[0])
 
 #question list, edit the text file and put line breaks between questions to add your own list
 f = open("questionList.txt","r",encoding="utf_8_sig")
